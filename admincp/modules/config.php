af50d0040e7b167aa2bb7c724dadf92a27bc36a4
minhsangtran <45291326+minhsangtran@users.noreply.github.com>
2020-05-07 22:32:34 +0700

    Update config.php

--- admincp/modules/config.php
+++ admincp/modules/config.php
@@ -1,8 +1,8 @@
 ﻿<?php
 	$tenmaychu='localhost';
 	$tentaikhoan='root';
-	$pass='';
-	$csdl='webphukiendt';
+	$pass='123456';
+	$csdl='shop';
 	$conn=mysql_connect($tenmaychu,$tentaikhoan,$pass,$csdl) or die('Ko kết nối được');
 	mysql_select_db($csdl,$conn);
 ?>
